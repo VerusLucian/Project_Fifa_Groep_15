182b791044072f8d2b4877f1b50b2c426c65c087
WaelALR <waelaf95@gmail.com>
2017-05-10 13:26:27 +0200

    v0.0

---
+++ website/public/templates/teams-toevoegen.php
@@ -0,0 +1,96 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	 
+</head>
+<style>
+.form-group input[type=text] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+.btn {
+    background-color: #135;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+section{
+    -webkit-column-count: 3; /* Chrome, Safari, Opera */
+    -moz-column-count: 3; /* Firefox */
+    column-count: 3;
+}
+
+	
+}
+</style>
+<body> -->
+<div class="container">
+    <div class="form-group">
+      <label for="team">Team toevoegen:</label>
+      <input type="text" class="form-control" id="team" placeholder="Enter team.." name="team">
+    </div>
+	<div class="form-group">
+	<section>
+      <label class="size" for="plyr">Player toevoegen:</label>
+      <input type="text" class="form-control" id="width" placeholder="Enter player name.." name="plyr">
+      <label class="size" for="plyr">Achternaam:</label>
+      <input type="text" class="form-control" id="width" placeholder="Enter last name.." name="achternm">
+      <label class="size" for="plyr">D-Code:</label>
+      <input type="text" class="form-control" id="width" placeholder="Enter code.." name="dcode">
+    </section>
+    </div>
+	<div class="form-group">
+	<section>
+      <label class="size" for="plyr">Player toevoegen:</label>
+      <input type="text" class="form-control" id="width" placeholder="Enter player name.." name="plyr">
+      <label class="size" for="plyr">Achternaam:</label>
+      <input type="text" class="form-control" id="width" placeholder="Enter last name.." name="achternm">
+      <label class="size" for="plyr">D-Code:</label>
+      <input type="text" class="form-control" id="width" placeholder="Enter code.." name="dcode">
+    </section>
+    </div>
+	<div class="form-group">
+	<section>
+      <label class="size" for="plyr">Player toevoegen:</label>
+      <input type="text" class="form-control" id="width" placeholder="Enter player name.." name="plyr">
+      <label class="size" for="plyr">Achternaam:</label>
+      <input type="text" class="form-control" id="width" placeholder="Enter last name.." name="achternm">
+      <label class="size" for="plyr">D-Code:</label>
+      <input type="text" class="form-control" id="width" placeholder="Enter code.." name="dcode">
+    </section>
+    </div>
+	<div class="form-group">
+	<section>
+      <label class="size" for="plyr">Player toevoegen:</label>
+      <input type="text" class="form-control" id="width" placeholder="Enter player name.." name="plyr">
+      <label class="size" for="plyr">Achternaam:</label>
+      <input type="text" class="form-control" id="width" placeholder="Enter last name.." name="achternm">
+      <label class="size" for="plyr">D-Code:</label>
+      <input type="text" class="form-control" id="width" placeholder="Enter code.." name="dcode">
+    </section>
+    </div>
+	<div class="form-group">
+	<section>
+      <label class="size" for="plyr">Player toevoegen:</label>
+      <input type="text" class="form-control" id="width" placeholder="Enter player name.." name="plyr">
+      <label class="size" for="plyr">Achternaam:</label>
+      <input type="text" class="form-control" id="width" placeholder="Enter last name.." name="achternm">
+      <label class="size" for="plyr">D-Code:</label>
+      <input type="text" class="form-control" id="width" placeholder="Enter code.." name="dcode">
+    </section>
+    </div>
+    
+    <button type="submit" class="btn">Submit</button>
+  </form>
+</div>
+<!-- </body>
+</html> -->
