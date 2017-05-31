768461d6f2c41ac897f3b734c3f39abad15340a1
VerusLucian <minelukasz@gmail.com>
2017-05-31 15:12:17 +0200

    Alpha 1.20
    Marge Fix

--- website/public/teams.php
+++ website/public/teams.php
@@ -8,17 +8,10 @@ $final = new FinalTabel($db);
 
 
     <div class="main-content">
-
-<<<<<<< Updated upstream
-        <a style="text-decoration: none" href=""><div class="box" style="width: 100px; height: 100px; border: solid 1px; border-radius: 10px;"><h1 style="font-size: 60px; text-align: center">+</h1></div></a>
-=======
         <?php
             include 'templates/teams-toevoegen.php';
             include 'templates/players-toevoegen.php';
         ?>
->>>>>>> Stashed changes
-
-
     </div>
 
 <?php require(realpath(__DIR__) . '/templates/footer.php'); ?>
