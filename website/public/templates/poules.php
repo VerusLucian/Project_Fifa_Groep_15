42744367560bf80e54043269d415f7cfcb12306a
VerusLucian <minelukasz@gmail.com>
2017-06-01 12:02:12 +0200

    Alpha 1.24

--- website/public/templates/poules.php
+++ website/public/templates/poules.php
@@ -11,14 +11,14 @@
             foreach ($poule->GetPoules() as $poule)
             {
                 $count ++;
-                echo '<div class="item"><li>';
+                echo '<div class="item">';
                 echo '<h3>' . $poule['naam'] . '</h3>';
                 echo '<section class="container text-center col-xs-12"><table class="table table-striped text-center"><thead><tr>';
-                echo '<th class="text-center col-xs-4">Team</th>' . '   <th class="text-center col-xs-4">Gewonnen</th>  ' . '  <th class="text-center col-xs-4">Verloren</th>  ' . '  <th class="text-center col-xs-4">Geijkspel</th>  ' . '  <th class="text-center col-xs-4">TeamScore</th>  ';
+                echo '<th class="text-center col-xs-4">Team</th>' . '   <th class="text-center col-xs-4">Gewonnen</th>  ' . '  <th class="text-center col-xs-4">Verloren</th>  ' . '  <th class="text-center col-xs-4">Geijkspel</th>  ' . '  <th class="text-center col-xs-4">TeamScore</th></tr>  ';
+                echo '</thead><tbody>';
                 foreach ($teams->GetTeamByPuleId($poule['id']) as $team)
                 {
-                    echo '</thead><tbody><td class="text-center col-xs-4">' . $team['name'] . '</td>' . '<td class="text-center col-xs-4">' . $team['win'] . '</td>' . '<td class="text-center col-xs-4">' . $team['lose'] . '</td>' . '<td class="text-center col-xs-4">' . $team['draw'] . '</td>' . '<td class="text-center col-xs-4">' . $team['score'] . '</td>';
-                    echo '</li>';
+                    echo '<tr><td class="text-center col-xs-4"><a href="team_info.php?team_id='.$team['id'].'">' . $team['name'] . '</a></td>' . '<td class="text-center col-xs-4">' . $team['win'] . '</td>' . '<td class="text-center col-xs-4">' . $team['lose'] . '</td>' . '<td class="text-center col-xs-4">' . $team['draw'] . '</td>' . '<td class="text-center col-xs-4">' . $team['score'] . '</td></tr>';
                 }
                 echo '</tbody></table></section><br></div>';
 
