c33ab1bc0d590a5a5ae0e2f873bf3d18c458372c
VerusLucian <minelukasz@gmail.com>
2017-06-06 16:27:44 +0200

    RECOVER

--- website/php/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-require_once 'init.php';
-$Login = new Login($db);
-$Login->logout();
-$message = "Je bent uitgelogd !";
-header("Location: ../public/index.php?message=$message&mode=msg");
--- website/public/templates/footer.php
+++
@@ -1,7 +0,0 @@
-</div>
-</div>
-  <footer>
-      <p>© 2017-2017 Fifa Dev Project All Rights Reserved</p>
-  </footer>
-</body>
-</html>
